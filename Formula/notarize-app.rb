614d9422a9c025023d85f9c495497e7399dcfc5f
Eddie Cameron <eddiec@grapefruitgames.com>
2022-04-08 09:42:27 +1200

    Update notarize-app.rb

--- Formula/notarize-app.rb
+++ Formula/notarize-app.rb
@@ -1,8 +1,8 @@
 class NotarizeApp < Formula
   desc "Make and notarise a .app or .dmg outside of Xcode"
   homepage "https://github.com/EddieCameron/notarize-app"
-  url "https://github.com/EddieCameron/notarize-app/releases/download/1.0.2/notarize-app-homebrew-1.0.2.tar.gz"
-  sha256 "9dd11038cf46e971bd07cd316519faf8a372f5e48ee25c9bd52c440c910c261a"
+  url "https://github.com/EddieCameron/notarize-app/releases/download/1.0.3/notarize-app-homebrew-1.0.3.tar.gz"
+  sha256 "ecf1d6285efcfd9ede6e8616acedbd2f39df713b9da65412508b923c3aebaf0f"
   license "MIT"
 
   depends_on "create-dmg"
