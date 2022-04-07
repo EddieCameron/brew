345ebb4252e43527229ff4f1869debaf5093c33c
Eddie Cameron <eddiec@grapefruitgames.com>
2022-04-08 08:57:59 +1200

    License

--- Formula/notarize-app.rb
+++ Formula/notarize-app.rb
@@ -3,6 +3,7 @@ class NotarizeApp < Formula
   homepage "https://github.com/EddieCameron/notarize-app"
   url "https://github.com/EddieCameron/notarize-app/releases/download/1.0.2/notarize-app-homebrew-1.0.2.tar.gz"
   sha256 "9dd11038cf46e971bd07cd316519faf8a372f5e48ee25c9bd52c440c910c261a"
+  license "MIT"
 
   depends_on "create-dmg"
 
